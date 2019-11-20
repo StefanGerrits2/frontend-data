e98074afdb1f8671b88c99e341fe1b2fa5c8f038
Stefan Gerrits <stefangerrits2000@gmail.com>
2019-11-20 17:27:40 +0100

    add start on legend, add css cursors

--- modules/drawCircles.js
+++ modules/drawCircles.js
@@ -15,46 +15,56 @@ export default function drawCircles(data) {
 
     const root = pack(data);
 
-    function onHover() {
-        d3.select(this).attr('class', 'startHover')
-    }
-
-    function onLeave() {
-        d3.select(this).attr('class', 'leaveHover')
-    }
-
     // Select svg
     const svg = d3.selectAll('svg')
         .attr('viewBox', [0, 0, width, height])
         .call(d3.zoom().scaleExtent([1 / 8, 24]).on('zoom', onzoom))
-        .append('g')
+        .append('g');
 
     // Create parent element for the circles and text
     const leaf = svg.selectAll('g')
         .data(root.leaves())
         .enter()
         .append('g')
-        .attr('transform', d => `translate(${d.x + 1},${d.y + 1})`)
+        .attr('transform', d => `translate(${d.x + 1},${d.y + 1})`);
+
     // Create bubbles
     leaf.append('circle')
         .attr('r', d => d.r)
-        .on('mouseenter', onHover)
-        .on('mouseout', onLeave)
     // Assign colors
         .attr('fill', d => color(d.data.upperCategory));
+    
+    // Create text
     leaf.append('text')
         .attr('x', 0)
         .attr('y', 0)
+        .attr('fill', 'white')
     // Make text invisible when the amount of the category is lower than 500
         .attr('display', d => {return d.data.categoryAmount <= 500 ? 'none' : 'flex';})
         .text(d => d.data.categoryName);
+
     // Add hover effect with data
     leaf.append('title')
         .text(d => `${d.data.categoryName}\n${format(d.data.categoryAmount)}\nCategorie: ${d.data.upperCategory}`);
 
+    // Zoom function
     function onzoom() {
-        svg.attr('transform', d3.event.transform);
+        svg.attr('transform', d3.event.transform)
     }
+
+    // Add legend
+    const legend = svg.selectAll('.legend')
+        .data(data)
+        .enter()
+        .append('g');
+    
+    legend.append('text')
+        .attr('fill', function(d){return color(d.upperCategory);})
+        .attr('y', function(d, i){return 32+20*i;})
+        .attr('x', -90)
+        .text(function (d) {
+            return d.upperCategory;
+        });
         
     return svg.node();
 }
